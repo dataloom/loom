680932474270369e11aa9826ad0c702e5aa78679
Matthew Tamayo-Rios <matthew@openlattice.com>
2017-07-16 13:45:50 -0700

    Add run script

---
+++ run.sh
@@ -0,0 +1,23 @@
+# Total available RAM: 62g
+# Conductor JVM settings
+CONDUCTOR_XMS = 40g
+CONDUCTOR_XMX = 40g
+
+# Datastore JVM settings
+DATASTORE_XMS = 12g
+DATASTORE_XMX = 12g
+
+#Neuron JVM settings
+NEURON_XMS = 4g
+NEURON_XMX = 4g
+
+#Total in use = 40 + 12 + 4 = 56g out of 62g
+
+nohup ./gradlew :conductor:run > /opt/conductor.log 2>&1& echo $! >conductor.pid
+sleep 20
+nohup ./gradlew :datastore:run > /opt/datastore.log 2>&1& echo $! >datastore.pid
+sleep 20
+nohup ./gradlew :datastore:neuron > /opt/neuron.log 2>&1& echo $! >datastore.pid
+sleep 10
+
+tail -f /opt/conductor.log /opt/datastore.log /opt/neuron.log
