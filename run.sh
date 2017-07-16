0de0ce0e01a21ecc928f6319f997c7b590e7611a
Matthew Tamayo-Rios <matthew@openlattice.com>
2017-07-16 16:57:16 -0700

    Update run.sh for functional logging

--- run.sh
+++ run.sh
@@ -18,11 +18,11 @@ export NEURON_ARGS="cassandra aws"
 
 #Total in use = 40 + 12 + 4 = 56g out of 62g
 
-nohup ./gradlew :conductor:run > /opt/conductor.log 2>&1& echo $! >conductor.pid
+nohup ./gradlew :conductor:run > /dev/null 2>&1& echo $! >conductor.pid
 sleep 20
-nohup ./gradlew :datastore:run > /opt/datastore.log 2>&1& echo $! >datastore.pid
+nohup ./gradlew :datastore:run > /dev/null 2>&1& echo $! >datastore.pid
 sleep 20
-nohup ./gradlew :datastore:neuron > /opt/neuron.log 2>&1& echo $! >datastore.pid
+nohup ./gradlew :datastore:neuron > /dev/null 2>&1& echo $! >datastore.pid
 sleep 10
 
-tail -f /opt/conductor.log /opt/datastore.log /opt/neuron.log
+tail -f ./logging/conductor.log ./datastore.log ./neuron.log
