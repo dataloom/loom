53c785893a309560ec0f8211248395cd4b046b47
Matthew Tamayo-Rios <matthew@openlattice.com>
2017-07-16 14:20:06 -0700

    Updates

--- run.sh
+++ run.sh
@@ -2,19 +2,19 @@
 # Total available RAM: 62g
 
 # Conductor JVM settings
-CONDUCTOR_XMS = 40g
-CONDUCTOR_XMX = 40g
-CONDUCTOR_ARGS = "cassandra spark aws"
+CONDUCTOR_XMS="40g"
+CONDUCTOR_XMX="40g"
+CONDUCTOR_ARGS="cassandra spark aws"
 
 # Datastore JVM settings
-DATASTORE_XMS = 12g
-DATASTORE_XMX = 12g
-DATASTORE_ARGS = "cassandra aws"
+DATASTORE_XMS="12g"
+DATASTORE_XMX="12g"
+DATASTORE_ARGS="cassandra aws"
 
 #Neuron JVM settings
-NEURON_XMS = 4g
-NEURON_XMX = 4g
-NEURON_ARGS = "cassandra aws"
+NEURON_XMS=4g"
+NEURON_XMX="4g"
+NEURON_ARGS="cassandra aws"
 
 #Total in use = 40 + 12 + 4 = 56g out of 62g
 
